08c3cb012bb036b2d581aa1b9c53dd49e785a976
Zeeek250 <damonbaker27@hotmail.com>
2023-01-22 16:49:46 -0600

    added to homeworld component

--- src/app/components/homeworlds/homeworlds.component.ts
+++ src/app/components/homeworlds/homeworlds.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
-import {home}
+import { Component, OnInit } from '@angular/core';
+import { HomeWorld } from 'src/app/models/homeworld.model';
+import { StarWarsService } from 'src/app/services/starwars.service';
 
 @Component({
   selector: 'app-homeworlds',
   templateUrl: './homeworlds.component.html',
   styleUrls: ['./homeworlds.component.css']
 })
-export class HomeworldsComponent {
+export class HomeworldsComponent implements OnInit {
 
+  constructor(private starWarsService: StarWarsService){}
+
+  homeWorlds : HomeWorld[]=[];
+
+  ngOnInit(): void {
+      this.starWarsService.getAllPlanets().subscribe((response)=>{
+        console.log(response);
+        this.homeWorlds = response;
+      })
+  }
 }
